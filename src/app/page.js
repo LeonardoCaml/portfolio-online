38149ed51c26b3fe70a5506b589ad105279f5f64
leonardo <leonardocamelo20@gmail.com>
2024-12-04 15:59:33 -0300

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,101 +1,98 @@
 import Image from "next/image";
 
+import title from "./assets/title.png";
+import javascript from "./assets/Javascript.png";
+import next from "./assets/nextjs.png";
+import redux from "./assets/redux.png";
+import react from "./assets/React.png";
+import tailwind from "./assets/Tailwind.png";
+import tela from "./assets/telainicial.png";
+
+import Navbar from "../components/Navbar";
+import { Container, Stack } from "@mui/material";
+
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <>
+      <Container
+        sx={{
+          height: "100vh",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-between",
+        }}
+      >
+        <Stack sx={{ display: "flex", justifyContent: "space-between" }}>
+          <Navbar />
+          <Image src={title} width={400} />
+        </Stack>
+        <Stack>
+          <Image
+            className="transition-all hover:-translate-y-4 duration-500"
+            src={javascript}
+            width={50}
+          />
+          ;
+        </Stack>
+      </Container>
+      <div className="h-svh flex flex-col items-center justify-center">
+        <div>
+          <h1 className="text-4xl">Sobre mim</h1>
+          <p className="text-xl w-[60rem]">
+            Sempre fui um entusiasta da tecnologia, desde muito cedo tive
+            contato com manuntenção de eletrônicos e outros programas de
+            computador. Porém, nunca encherguei o mercado da tecnologia como uma
+            profissão, apenas como um passa tempo, visão essa que foi mudando ao
+            longo dos anos.
+            <br></br>
+            <br></br>
+            Iniciei minha jornada no desenvolvimento web somente em 2021 após
+            conhecer de perto como funcionava a estrutura de um site e as
+            tecnologias que rodavam por trás de uma aplicação. Desde então,
+            comecei a desenvolver minhas próprias landing pages e aperfeiçoar
+            minhas skills como desenvolvedor Front-end afim de me tornar um
+            excelente profissional.
+            <br></br>
+            <br></br>
+            Em 2022 tive meu primeiro cliente, dono de uma empresa de
+            contabilidade que precisava de uma landing page para exibir seus
+            serviços e apresentar sua historia para potenciais clientes. Foi uma
+            excelente oportunidade de aplicar meus conhecimentos e compreender
+            onde precisava melhorar
+            <br></br>
+            <br></br>
+            Hoje, busco atuar como desenvolvedor front-end e, talvez em alguns
+            anos, começar a liderar meu proprio time de desenvolvedores
+          </p>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </div>
+      <div className="h-svh flex flex-col items-center justify-evenly">
+        <h1 className="text-8xl">Projetos</h1>
+        <div className="flex gap-32">
+          <div className="w-80 h-96  border-2">
+            <div>
+              <Image src={tela} className="w-full h-56 object-cover" />
+            </div>
+            <div className="p-5 bg-gray-300">
+              <h1 className="text-2xl">título</h1>
+              <p className="text-lg">descrição basica do projeto</p>
+              <p className="text-lg">link: www.site-exemplo.com.br</p>
+            </div>
+          </div>
+
+          <div className="bg-gray-300 w-80 h-96 overflow-hidden border-2 border-black">
+            <div className="bg-white w-full h-56 border-black"></div>
+            <div className="p-5">
+              <h1 className="text-2xl">título</h1>
+              <p className="text-lg">descrição basica do projeto</p>
+              <p className="text-lg">link: www.site-exemplo.com.br</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="h-svh flex flex-col items-center justify-center">
+        <h1>Envie sua mensagem</h1>
+      </div>
+    </>
   );
 }
